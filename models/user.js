7eca36485065fe1281e7681f1bf93a2e31743585
markobezic <marko.bezic@gmail.com>
2021-04-26 11:10:48 -0400

    add user routes

--- models/user.js
+++ models/user.js
@@ -8,15 +8,51 @@ module.exports = (sequelize, DataTypes) => {
     {
       firstName: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "a first name is required",
+          },
+          notEmpty: {
+            msg: "please provide a first name",
+          },
+        },
       },
       lastName: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "a last name is required",
+          },
+          notEmpty: {
+            msg: "please provide a last name",
+          },
+        },
       },
       emailAddress: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "an email address is required",
+          },
+          notEmpty: {
+            msg: "please provide an email address name",
+          },
+        },
       },
       password: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "a password name is required",
+          },
+          notEmpty: {
+            msg: "please provide a passwprd name",
+          },
+        },
       },
     },
     { sequelize }
--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,16 @@ const { asyncHandler } = require("../middleware/async-handler");
 // Construct a router instance
 const router = express.Router();
 
+// body parser
+router.use(express.json());
+
 /* GET current user */
 router.get(
   "/users",
   asyncHandler(async (req, res) => {
     const users = await User.findAll();
     res.json(users);
+    res.status(200);
   })
 );
 
@@ -18,7 +22,23 @@ router.get(
 router.post(
   "/users",
   asyncHandler(async (req, res) => {
-    await User.create(req.body);
+    try {
+      await User.create(req.body);
+      res.location("/");
+      res.status(201).json({ message: "User successfully created!" });
+    } catch (error) {
+      console.log("ERROR:", error.name);
+
+      if (
+        error.name === "SequlizeValidationError" ||
+        error.name === "SequelizeConstraintError"
+      ) {
+        const errors = error.errors.map((err) => err.message);
+        restart.status(400).json({ errors });
+      } else {
+        throw error;
+      }
+    }
   })
 );
 
